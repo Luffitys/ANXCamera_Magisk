e7b5031f2909ff2a59e647840c53336bab70ff26
luffitys <banackidaniel@gmail.com>
2019-10-31 18:10:17 +0100

    Optimized checks, removed libcameraservice.so

--- common/service.sh
+++ common/service.sh
@@ -1,5 +1,2 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
-
-sleep 6
-appops set com.android.camera android:legacy_storage allow
--- common/unity_install.sh
+++ common/unity_install.sh
@@ -2,46 +2,46 @@ DEVICENAME=$(getprop ro.product.device)
 MODULEVERSION=$(grep_prop versionCode $TMPDIR/module.prop)
 ANDROIDVERSION=$(getprop ro.system.build.version.release)
 
-ui_print ""
+ui_print " "
 ui_print "     ─────────────────────────────────────"
 ui_print "                 [ANXCamera v$MODULEVERSION]"
 ui_print "         Your device is recognized as `toupper $DEVICENAME`"
 ui_print "                 Android Version $ANDROIDVERSION!"
 #Android Version Check
-if [ $ANDROIDVERSION == 9 ]; then
-	ui_print ""
+if [ $ANDROIDVERSION != 10 ]; then
+	ui_print " "
 	ui_print "           Android 9 (Pie) is discontinued!"
 	ui_print "             Latest Pie version is v151!"
 	ui_print "            Please download above instead!"
-	ui_print ""
+	ui_print " "
 	ui_print "            Installation will be aborted!"
 	ui_print "     ─────────────────────────────────────"
 	abort
 fi
 # Miui Check
 if [ -f /system/app/miui/miui.apk ]; then
-	ui_print ""
+	ui_print " "
 	ui_print "                 Miui ROM detected!"
 	ui_print "               Miui is NOT supported!"
-	ui_print ""
-	ui_print "            Installation will be aborted!"
+	#ui_print " "
+	#ui_print "            Installation will be aborted!"
 	ui_print "     ─────────────────────────────────────"
-	abort
+	#abort
 fi
-ui_print ""
+ui_print " "
 ui_print "                Created by Luffitys,"
 ui_print "                       AEonAX,"
 ui_print "               CodeElixir, Abhishek987"
 ui_print "                     Wolf, Noid"
 ui_print "            and many other great people"
 ui_print "     ─────────────────────────────────────"
-ui_print ""
+ui_print " "
 
 # Device Check
-if [ $(getprop ro.product.device) == davinci ] || [ $(getprop ro.product.device) == davinciin ] || [ $(getprop ro.product.device) == raphael ] || [ $(getprop ro.product.device) == raphaelin ] || [ $(getprop ro.product.device) == cepheus ] || [ $(getprop ro.product.device) == perseus ] || [ $(getprop ro.product.device) == violet ] || [ $(getprop ro.product.device) == whyred ]; then
-	rm $MODPATH/system/lib/libcameraservice.so
-	rm $MODPATH/system/lib64/libcameraservice.so
-fi
+#if [ $(getprop ro.product.device) == davinci ] || [ $(getprop ro.product.device) == davinciin ] || [ $(getprop ro.product.device) == raphael ] || [ $(getprop ro.product.device) == raphaelin ] || [ $(getprop ro.product.device) == cepheus ] || [ $(getprop ro.product.device) == perseus ] || [ $(getprop ro.product.device) == violet ] || [ $(getprop ro.product.device) == whyred ]; then
+	#rm $MODPATH/system/lib/libcameraservice.so
+	#rm $MODPATH/system/lib64/libcameraservice.so
+#fi
 
 
 MODULENAME=$(grep_prop name $TMPDIR/module.prop)
