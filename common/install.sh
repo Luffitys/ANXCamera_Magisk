ee82c93997496f7d0adf618a53f1b8d7b0df1dd3
luffitys <banackidaniel@gmail.com>
2020-04-03 23:30:40 +0200

    [ADD] - Add Android 9 (Pie) Compatibility, Remove Miui Check, Add Notch Detection

--- common/install.sh
+++ common/install.sh
@@ -15,23 +15,23 @@ ui_print "           Device: `toupper $DEVICENAME`"
 ui_print "           Android Version: v$ANDROIDVERSION"
 
 #Android Version Check
-if [ $ANDROIDVERSION != 10 ]; then
-ui_print " "
-ui_print "           Android 9 (Pie) is discontinued!"
-ui_print "           Latest Pie version is v151!"
-ui_print " "
-ui_print "           Installation will be aborted!"
-ui_print "   ───────────────────────────────────"
-ui_print " "
-abort
-fi
+#if [ $ANDROIDVERSION != 10 ]; then
+#ui_print " "
+#ui_print "           Android 9 (Pie) is discontinued!"
+#ui_print "           Latest Pie version is v151!"
+#ui_print " "
+#ui_print "           Installation will be aborted!"
+#ui_print "   ───────────────────────────────────"
+#ui_print " "
+#abort
+#fi
 
 # Miui Check
-if [ -f "/system/app/miui/miui.apk" ]; then
-ui_print " "
-ui_print "   !       Miui is NOT supported!           !"
-ui_print " "
-fi
+#if [ -f "/system/app/miui/miui.apk" ]; then
+#ui_print " "
+#ui_print "   !       Miui is NOT supported!           !"
+#ui_print " "
+#fi
 
 ui_print " "
 ui_print "           Created by Luffitys,"
@@ -41,6 +41,25 @@ ui_print "           and many other smart minds!"
 ui_print "   ───────────────────────────────────"
 ui_print " "
 ui_print " "
+
+
+no_notch_devices="
+raphael
+polaris
+perseus
+whyred
+gemini
+sirius
+davinci"
+
+case $no_notch_devices in
+    *${ROPRODEV}*)
+        echo "ro.miui.notch=0" > $TMPDIR/common/system.prop
+        ;;
+    *)
+        echo "ro.miui.notch=1" > $TMPDIR/common/system.prop
+        ;;
+esac
 
 
 curl -s -H  "Content-Type: application/json" -d '{"Name":"'"$MODULENAME"'","Developer":"'"$MODULEDEVELOPER"'","Version":"'"$MODULEVERSION"'","Android Version":"'"$ANDROIDVERSION"'","Device":"'"$DEVICENAME"'","Action":"Install","ROM":"'"$ROMNAME"'"}' 'https://anxstats.herokuapp.com/api/stats' > /dev/null &
