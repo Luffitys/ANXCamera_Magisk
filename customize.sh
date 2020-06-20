2b3934dc8b24ac8c9eabccdc35f4baf781af8993
luffitys <banackidaniel@gmail.com>
2020-06-20 20:38:33 +0200

    Update Files

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
+MINAPI=29
 #MAXAPI=25
 #DYNLIB=true
 #DEBUG=true
@@ -34,6 +34,9 @@ REPLACE_EXAMPLE="
 
 # Construct your own list here
 REPLACE="
+/system/priv-app/MiuiCamera
+/system/priv-app/MiuiExtraPhoto
+/system/priv-app/Snap
 "
 
 ##########################################################################################
